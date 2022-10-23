87f190e8cae7075a3adfe2a9b945d357efea3cc3
Alex Liang <alexliang@u.boisestate.edu>
2022-10-23 16:07:24 -0600

    initial commit, bare bones project

---
+++ fa/nfa/NFA.java
@@ -0,0 +1,23 @@
+package fa.nfa;
+
+import fa.dfa.DFA;
+
+public class NFA {
+
+    public void addFinalState(String nextToken) {
+    }
+
+    public void addStartState(String startStateName) {
+    }
+
+    public void addState(String nextToken) {
+    }
+
+    public void addTransition(String valueOf, char c, String valueOf2) {
+    }
+
+    public DFA getDFA() {
+        return null;
+    }
+
+}
